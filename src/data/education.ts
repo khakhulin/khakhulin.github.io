719e9c6e430ef59e0b8d967ecff7024b7927d50b
Taras Khakhulin <t.khakhulin@gmail.com>
2025-07-28 00:42:17 +0100

    upd education

--- src/data/education.ts
+++ src/data/education.ts
@@ -2,6 +2,9 @@ export interface Education {
   year: string;
   degree: string;
   institution: string;
+  advisor?: string;
+  thesis?: string;
+  thesisUrl?: string;
   description?: string;
   location?: string;
 }
